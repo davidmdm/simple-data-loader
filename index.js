e04e15881e904aa038990074743ef44ca839b3a3
davidmdm <david.desmarais-michaud@pj.ca>
2018-08-30 08:41:33 -0400

    made module exportable again

--- index.js
+++ index.js
@@ -1,51 +1,8 @@
 'use strict';
 
-const set = (cache, keys, result) => {
-  if (keys.length === 1) {
-    return cache.set(keys[0], result);
-  }
-  if (!cache.has(keys[0])) {
-    cache.set(keys[0], new Map());
-  }
-  const nextCache = cache.get(keys[0]);
-  set(nextCache, keys.slice(1), result);
-};
+const { set, has, get, del } = require('./utils');
 
-const has = (cache, keys) => {
-  if (keys.length === 1) {
-    return cache.has(keys[0]);
-  }
-  if (!cache.has(keys[0])) {
-    return false;
-  }
-  const nextCache = cache.get(keys[0]);
-  return has(nextCache, keys.slice(1));
-};
-
-const get = (cache, keys) => {
-  if (keys.length === 1) {
-    return cache.get(keys[0]);
-  }
-  const nextCache = cache.get(keys[0]);
-  return get(nextCache, keys.slice(1));
-};
-
-const del = (cache, keys) => {
-  if (keys.length === 1) {
-    return cache.delete(keys[0]);
-  }
-  if (!cache.has(keys[0])) {
-    return false;
-  }
-  const nextCache = cache.get(keys[0]);
-  const res = del(nextCache, keys.slice(1));
-  if (Array.from(nextCache.keys()).length === 0) {
-    cache.delete(keys[0]);
-  }
-  return res;
-};
-
-function simpleLoader(fn, opts = {}) {
+module.exports = function dataloader(fn, opts = {}) {
   if (typeof fn === 'object') {
     opts = fn;
     fn = opts.fn;
@@ -65,7 +22,6 @@ function simpleLoader(fn, opts = {}) {
       const keys = args.slice(0, arity);
 
       if (has(cache, keys)) {
-        console.log([...cache.entries()]);
         return get(cache, keys);
       }
 
@@ -82,8 +38,6 @@ function simpleLoader(fn, opts = {}) {
           }, opts.ttl)
         );
       }
-
-      console.log([...cache.entries()]);
       return promise;
     },
 
@@ -93,23 +47,7 @@ function simpleLoader(fn, opts = {}) {
         clearTimeout(get(timeouts, keys));
         del(timeouts, keys);
       }
-      const res = del(cache, keys);
-
-      console.log([...cache.entries()]);
-
-      return res;
+      return del(cache, keys);
     },
   };
-}
-
-const multiply = (x, y) => x * y;
-
-const multLoader = simpleLoader(multiply, { ttl: 2000 });
-
-const p1 = multLoader.load(5, 6);
-const p2 = multLoader.load(5, 7);
-
-setTimeout(() => {
-  multLoader.load(10, 3);
-  multLoader.load(4, 5);
-}, 3000);
+};
