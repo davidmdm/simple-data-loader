44e0bdcea9dc44ae6d262c678ecf3e4d65b8df9d
dmichaud <dmichaud@lvlstudio.com>
2018-09-13 11:31:14 -0400

    Updated index.d.ts again

--- index.d.ts
+++ index.d.ts
@@ -11,11 +11,13 @@ declare namespace dataloader {
     load?: Function;
   }
 
+  type UnwrapPromise<T extends Promise<any>> = T extends Promise<infer K> ? K : never;
+  type UnwrapReturnType<T extends (...args: any[]) => any> = T extends (...args: any[]) => Promise<infer K> ? K : ReturnType<T>;
   type ArgumentsType<T> = T extends  (...args: infer U) => any ? U: never;
 
   interface ResultInterface<T extends (...args: any[]) => any> {
     delete: (...args: ArgumentsType<T>) => boolean;
-    load: (...args: ArgumentsType<T>) => Promise<ReturnType<T>>;
+    load: (...args: ArgumentsType<T>) => Promise<UnwrapReturnType<T>>;
   }
 
   export type SDLoader<T extends (...args: any[]) => any> = (fn: T, opts?: LoaderOptions) => ResultInterface<T>;
